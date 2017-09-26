6c22bf327b6b289daf8b52bee7ec74e5062046df
Xu Feng <xufeng1023@hotmail.com>
2017-09-25 17:50:06 -0700

    'nothing'

---
+++ tests/Feature/StripeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StripeTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
